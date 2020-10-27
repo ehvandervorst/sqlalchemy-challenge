16f041e23b9a08302ea9c03cfc825abd2d2de16d
Herrity <eherrity@wm.edu>
2020-10-26 20:53:08 -0700

    still working on flask

--- app.py
+++ app.py
@@ -1,33 +1,91 @@
-Now that you have completed your initial analysis, design a Flask API based on the queries that you have just developed.
-Use Flask to create your routes.
-/
-Home page.
-List all routes that are available.
+from flask import Flask
+app = Flask(__name__)
 
-/api/v1.0/precipitation
-Convert the query results to a dictionary using date as the key and prcp as the value.
-Return the JSON representation of your dictionary.
+#Home page. List all routes that are available.
+@app.route("/")
+def home():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
 
-/api/v1.0/stations
-Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    # Return the JSON representation of your dictionary.
+    session = Session(engine)
 
-/api/v1.0/tobs
-Query the dates and temperature observations of the most active station for the last year of data.
-Return a JSON list of temperature observations (TOBS) for the previous year.
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
 
-/api/v1.0/<start> and /api/v1.0/<start>/<end>
-Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-endrange.
-When given the start only, calculate
-TMIN
-,
-TAVG
-, and
-TMAX
-for all dates greater than and equal to the start date.
-When given the start and the end date, calculate the
-TMIN
-,
-TAVG
-, and
-TMAX
-for dates between the start and end date inclusive.
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Return a JSON list of stations from the dataset.
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #Query the dates and temperature observations of the most active station for the last year of data.
+    #eturn a JSON list of temperature observations (TOBS) for the previous year.
+
+@app.route("/api/v1.0/<start>")
+def start():
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-endrange.
+#When given the start only, calculate TMIN, TAVG and TMAX for all dates greater than and equal to the start date.
+
+@app.route("/api/v1.0/<start>/<end>")
+def start():
+#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+@app.route("/api/v1.0/passengers")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+
+
+
+
+
+
+
+
